e609c6c0fa1bfbb68edbf211d9c2634232019a28
kogara13 <kieranogara@gmail.com>
2022-04-29 21:50:36 -0400

    fixed directory issue and updated README

--- README.md
+++ README.md
@@ -2,16 +2,14 @@
 
 Terimal-based to-do list that takes command line arguments
 
-- Tasks are stored in ".todoList.txt" in the same directory. If it the file does not exists, adding a task will create it.
+- Tasks added to the list are stored in "~/.config/todo/list". If the directory or file does not exists, They will be creating after first running the progrm 
 
 Commands:
 
-- -a    Creates ".todoList.txt" in the same directory, adds the following task to the list, and displays the list ("todo.py -a Finish work")
+- -a    Adds the following command line arguments to the list as a task and displays the list ("todo.py -a Finish work")
 
 - -r    Removes task from the list based on its numbered position and displays the list ("todo.py -r 2" removes the second task in the list)
 
 - -l    Lists the current tasks in the list ("todo.py -l")
 
 - -c    Clears the list ("todo.py -c")
-
-
--- todo-list/todo.py
+++ todo-list/todo.py
@@ -6,43 +6,54 @@ import getpass
 #Get user for file placement
 user = getpass.getuser()
 
+#Create directory in ~/.cache and the to-do list file
+path = "/home/" + user + "/.cache/todo/"
+if not os.path.isdir(path):
+    os.mkdir(path)
+    f = open("/home/" + user + "/.cache/todo/list", "w+")
+    f.close()
+
 # Lists the tasks currently in the list
 def listTasks():
-    f = open("/home/" + user + "/.cache/todo-list/todoList.txt", "r")
+    f = open("/home/" + user + "/.cache/todo/list", "r")
     Lines = f.readlines()
-    for count, line in enumerate(Lines):
-        print("{}- {}".format(count + 1, line.strip()))
-    f.close()
+    if len(Lines) == 0:
+        print("All tasks complete!")
+    else:
+        for count, line in enumerate(Lines):
+            print("{}- {}".format(count + 1, line.strip()))
+        f.close()
 
 # Adds task to the file
 def addToFile(text):
-    f = open("/home/" + user + "/.cache/todo-list/todoList.txt", "a")
+    f = open("/home/" + user + "/.cache/todo/list", "a")
     f.write("{}\n".format(text))
     f.close()
     
 # Deletes task from the file based on what line of the file it is on
-def delete(deleteLine):
-    try:
-        with open("/home/" + user + "/.cache/todo-list/todoList.txt", 'r') as read:
-            Lines = read.readlines()
-            ptr = 1
-            with open("/home/" + user + "/.cache/todo-list/todoList.txt", 'w') as write:
-                for line in Lines:
-                    if ptr != deleteLine:
-                        write.write(line)
-                    else:
-                        removed = line
-                    ptr += 1
-            print("Finished Task: {}".format(removed))        
-    except:
-        print("error")
+def delete(deleteLine):    
+    with open("/home/" + user + "/.cache/todo/list", 'r') as read:
+        Lines = read.readlines()
+        ptr = 1
+        with open("/home/" + user + "/.cache/todo/list", 'w') as write:
+            for line in Lines:
+                if ptr != deleteLine:
+                    write.write(line)
+                else:
+                    removed = line
+                ptr += 1
+        print("Finished Task: {}".format(removed))        
+        if len(Lines) == 0:
+            print("All tasks complete!")
 
 # Clears the list
 def clear():
-    f = open("/home/" + user + "/.cache/todo-list/todoList.txt", "r+")
+    f = open("/home/" + user + "/.cache/todo/list", "r+")
     f.truncate(0)
     f.close()
+    print("Tasks cleared")
 
+#Command line arguments to call functions
 try:
     if sys.argv[1] == "-a":
         x = str(sys.argv[2])
